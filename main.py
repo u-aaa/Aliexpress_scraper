05bc5de99645a647b531cba07ee43ea6d04d10d1
Blessing E-Philips <47270361+u-aaa@users.noreply.github.com>
2021-08-21 20:57:57 +0300

    updated main file

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from src.database.database import Database
 
 
 sample = Aliexpress()
-df_m_shoes = sample.run_scraper('dog', 2000)
+df_m_shoes = sample.run_scraper('male shoes', 2000)
 sample.to_csv(df_m_shoes)
 
 sample_db = Database()
 sample_db.insert_data(df_m_shoes)
-db_m_shoes = sample_db.select_data('dog')
+db_m_shoes = sample_db.select_data('male shoes')
 print(db_m_shoes.head(20))
